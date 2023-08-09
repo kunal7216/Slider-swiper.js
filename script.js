259542ea90d8a1b0544e4b998d43c6cfda05a6b5
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2023-08-09 19:39:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "1",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: false
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    }
+  }
+});
